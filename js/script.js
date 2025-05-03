dd33f0d4fe79487d7be713e936db4591b2cff4d0
Excris <cristhianerik.2019@gmail.com>
2025-05-03 19:21:55 -0300

    altera

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+
+const size = 30;
+
+const snake = [
+    {x: 200, y: 200},
+    {x: 230, y: 200},
+]
+
+const DrawSnake = () => {
+    ctx.fillStyle = "#ddd";
+    
+    snake.forEach((position) => {
+        ctx.fillRect(position)    
+    })
+    
+}
+
+DrawSnake();
