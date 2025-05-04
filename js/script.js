3775b72e36801d5a12492d0cc525b7aa3c32fa42
Excris <cristhianerik.2019@gmail.com>
2025-05-04 19:43:30 -0300

    Game Completo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
+const audio = new Audio('../assets/audio.mp3.mp3')
+
 
 const size = 30;
+const inicialPosition = {x: 270, y: 240}
 
-const snake = [{x: 270, y: 240}]
+let snake = [inicialPosition]
+
+const incrementScore = () => {
+    score.innerText = +score.innerText + 10
+}
 
 const randomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min ) + min) 
@@ -94,7 +106,50 @@ const drawGrid = () => {
 }
 
 const checkEat = () => {
-    const heat = snake[snake.length - 1]
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        incrementScore()
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+const checkCollision = () => {
+    const head = snake[snake.length - 1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length - 2
+    const wallCollision = 
+        head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+    })
+       
+
+    if (wallCollision || selfCollision) {
+        gameOver()
+    }        
+}
+
+const gameOver = () => {
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerHTML
+    canvas.style.filter = "blur(2px)"
 }
 
 const gameLoop = () => {
@@ -105,6 +160,8 @@ const gameLoop = () => {
     drawFood()
     moveSnake()
     DrawSnake()
+    checkEat()
+    checkCollision()
 
     loopId = setTimeout(() => {
         gameLoop()
@@ -127,4 +184,12 @@ document.addEventListener("keydown", ({key}) => {
         direction = "up"
     }
     
+})
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00"
+    menu.style.display = "none"
+    canvas.style.filter = "none"
+
+    snake = [inicialPosition]
 })
